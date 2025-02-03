12225b91eb8e60ef1325b0ed8b6cb2418468b1b5
Andualem <andualem.fereja12@gmail.com>
2025-02-03 22:55:21 +0300

    product endpoint created

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,5 +1,7 @@
 import { PrismaClient } from "@prisma/client"
 import { NextRequest, NextResponse } from "next/server"
+import fs from 'fs'
+import path from 'path'
 
 export async function GET(request: NextRequest) {
     try {
@@ -25,6 +27,9 @@ export async function GET(request: NextRequest) {
                 price: true,
                 quantity: true,
                 imagePath: true
+            },
+            orderBy: {
+                createdAt: "desc"
             }
         })
 
@@ -44,5 +49,62 @@ export async function GET(request: NextRequest) {
         })
     } catch(e) {
         return NextResponse.json({ error: e })
+    }
+}
+
+async function upload(file:File): Promise<string> {
+    const uploadDir = path.join(process.cwd(), 'public', 'uploads')
+    if (!fs.existsSync(uploadDir)) {
+        fs.mkdirSync(uploadDir,{
+            recursive: true
+        })
+    }
+    const fileName = `${Date.now()}-${file.name}`
+    const filePath = path.join(uploadDir, fileName)
+
+    const buffer = Buffer.from(await file.arrayBuffer())
+    fs.writeFileSync(filePath, buffer)
+
+    return `/uploads/${fileName}`
+}
+
+export async function POST(request:NextRequest) {
+    try{
+        const formData = await request.formData()
+
+        const categoryId = parseInt(formData.get("category") as string)
+        const name = formData.get("name") as string
+        const description = formData.get("description") as string
+        const price = parseFloat(formData.get("price") as string)
+        const quantity = parseInt(formData.get("quantity") as string)
+        const image = formData.get("image") as File
+
+        const imagePath = await upload(image)
+        console.log("The file path is ---- ", imagePath)
+
+        const prisma = new PrismaClient()
+
+        const product = await prisma.product.create({
+            data: {
+                categoryId: categoryId,
+                name,
+                description,
+                price,
+                quantity,
+                imagePath
+            }
+        })
+
+        return NextResponse.json({
+            product: product
+        },{
+            status: 201
+        })
+    } catch (e) {
+        return NextResponse.json({
+            error: "Error while creating product."
+        }, {
+            status: 500
+        })
     }
 }
