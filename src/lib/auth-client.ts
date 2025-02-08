ab62f07a27fa9ea1a9512ec68d4044234e29c009
Andualem <andualem.fereja12@gmail.com>
2025-02-09 00:03:12 +0300

    modified

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,8 +1,7 @@
 import { createAuthClient } from "better-auth/react"
 import { adminClient } from "better-auth/client/plugins"
 
-const authClient = createAuthClient({
-    baseURL: "http://localhost:3000", // the base url of your auth server
+const authClient = createAuthClient({ // the base url of your auth server
     plugins: [
         adminClient()
     ]
