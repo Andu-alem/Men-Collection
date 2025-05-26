b56e0d52271bf7a5b63c9d1ae9eefdb81e4edd8b
Andualem <andualem.fereja12@gmail.com>
2025-05-26 16:17:48 +0300

    fix(auth): update auth related page layout

--- src/app/(auth)/auth/layout.tsx
+++ src/app/(auth)/auth/layout.tsx
@@ -1,45 +1,12 @@
-'use client'
-import Link from "next/link"
-import Image from "next/image"
-import { useEffect } from "react"
-import { useRouter, useSearchParams } from 'next/navigation'
-import { authClient } from "@/lib/auth-client"
-
+import { Suspense } from "react"
 
 export default function Layout({ children }:{ children:React.ReactNode }) {
-    const { isPending, data } = authClient.useSession()
-    const router = useRouter()
-    const searchParams = useSearchParams()
-
-    useEffect(() => {
-        if (!isPending && data) {
-            const callback = searchParams.get("callback")
-            if (callback) {
-                router.replace(callback)
-            } else {
-                router.replace("/products")
-            }
-        }
-    },[isPending])
-
-    if (isPending || data) {
-        return (
-            <div className="w-screen h-screen flex justify-center items-center">
-                <p className="text-lg font-bold text-zinc-700">Loading....</p>
-            </div>
-        )
-    }
 
     return (
-        <main className="h-screen w-screen bg-[url('/background/bg3.jpg')] bg-cover">
-            <div className="h-screen w-screen backdrop-blur-3xl flex flex-col justify-center items-center">
-                <div className="flex justify-center items-center mb-7">
-                    <Link className="" href="/products">
-                        <Image src="/logo.png" alt="logo" width={100} height={100} />
-                    </Link>
-                </div>
+        <Suspense>
+            <main className="min-h-screen w-screen bg-[url('/background/bg3.jpg')] bg-cover">
                 { children }
-            </div>
-        </main>
+            </main>
+        </Suspense>
     )
 }
