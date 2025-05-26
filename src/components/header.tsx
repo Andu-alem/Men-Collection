6182a27b7b1df23fabe16ea5f211b451701aa0f0
Andualem <andualem.fereja12@gmail.com>
2025-05-26 13:38:14 +0300

    feat(app): Add header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,19 +1,49 @@
+'use client'
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+import Search from "./SearchBar"
+import { ShoppingCart } from "lucide-react"
+import { Button } from "./ui/button"
+import { authClient } from "@/lib/auth-client"
+import { Skeleton } from "./ui/skeleton"
+import { Badge } from "./ui/badge"
+import { useCartStore } from "@/stores/store"
+
+
 export default function Header() {
+    const { data, isPending } = authClient.useSession()
+    const totalItems = useCartStore((state) => state.totalItems)
+    const pathName = usePathname()
+
     return (
-        <div className="fixed z-50 w-full p-2 flex flex-row justify-between text-white backdrop-blur-sm py-2 px-5">
-            <div>
-                <h1 className="text-lg font-bold">Class Men's Collection</h1>
+        <div className="z-50 sticky top-0 w-full p-2 flex flex-col gap-3 backdrop-blur-3xl pt-4 pb-2 px-5 border-b border-zinc-300">
+            <div className="flex flex justify-between">
+                <h1 className="text-xl font-bold">Men's Collection</h1>
+                <div className="hidden sm:block">
+                    <Search />
+                </div>
+                <div className="flex items-center gap-3">
+                    <Link href="/cart" className="relative">
+                        <ShoppingCart className="text-orange-500 font-bold" size="32" />
+                        {  totalItems > 0 && <Badge className="absolute top-0 right-0 z-50 text-[11px] rounded-full py-0 px-[3px] bg-green-500">{ totalItems }</Badge>}
+                    </Link>
+                    {
+                        isPending ? (
+                            <Skeleton className="h-7 rounded-md w-[40px]" />
+                        ) : data ? (
+                            <Button 
+                                className="h-7 text-[14px]" 
+                                onClick={() => authClient.signOut() }>LOGOUT</Button>
+                        ):(
+                            <Button className="h-7" asChild>
+                                <Link className="text-[14px]" href={`/auth/login?callback=${pathName}`}>LOGIN</Link>
+                            </Button>
+                        )
+                    }
+                </div>
             </div>
-            <div className="flex flex-row gap-3 text-[17px]">
-                <div>
-                    Products
-                </div>
-                <div>
-                    About-Us
-                </div>
-                <div>
-                    Contact-Us
-                </div>
+            <div className="block sm:hidden px-[7%]">
+                <Search />
             </div>
         </div>
     );
