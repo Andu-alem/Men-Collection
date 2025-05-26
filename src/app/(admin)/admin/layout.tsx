6ef03210f0e7964ff720eaa1b53029d5fb08fa62
Andualem <andualem.fereja12@gmail.com>
2025-05-26 15:22:09 +0300

    feat(admin): Update the admin layout

--- src/app/(admin)/admin/layout.tsx
+++ src/app/(admin)/admin/layout.tsx
@@ -1,4 +1,4 @@
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { SidebarProvider } from "@/components/ui/sidebar"
 import AdminSidebar from "@/components/admin-ui/AdminSideBar"
 
 export default function Layout({ children }:{ children: React.ReactNode }) {
