60489856919c20a08c168516683e434632730f55
Andualem <andualem.fereja12@gmail.com>
2025-05-26 17:07:26 +0300

    feat(home-page): update with magic ui dot background

--- src/components/HeroOutro.tsx
+++ src/components/HeroOutro.tsx
@@ -1,20 +1,31 @@
 'use client'
+import { cn } from "@/lib/utils"
+import { DotPattern } from "./magicui/dot-pattern"
 import { Button } from "./ui/button"
 import Link from "next/link"
+import { ChevronRightIcon } from "lucide-react"
 
 const HeroOutro = () => {
     return (
-        <div className="h-screen w-full bg-[url('/background/collection.jpg')] bg-cover relative">
+        <div className="relative h-screen w-full bg-[url('/background/collection.jpg')] bg-cover relative">
             <div className="w-full h-screen top-0 bg-gradient-to-l from-black via-black to-transparent"></div>
             <div className="absolute top-0 w-full h-full flex flex-col justify-center items-center">
                 <p 
-                    className="text-[30px] text-orange-500 w-10/12 sm:w-2/5 my-5 text-center"
-                >Enjoy our <span className="text-white font-bold">quality & lexury</span> suits, ties, shoes, watches, casual wearables, sunglasses, and perfumes.</p>
+                    className="text-[20px] md:text-[30px] text-orange-500 w-10/12 sm:w-3/5 my-5 text-center tracking-wide font-light leading-7"
+                >Enjoy our <span className="text-white">Quality & Lexury</span> suits, ties, shoes, watches, casual wearables, sunglasses, and perfumes.</p>
 
-                <Button variant="outline" className="text-white bg-black" asChild>
-                    <Link href="/products">Shop Now</Link>
+                <Button variant="outline" className="text-white bg-black group" asChild>
+                    <span className="flex">
+                        <Link href="/products">Shop Now</Link>
+                        <ChevronRightIcon className="text-orange-400 ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-1" />
+                    </span>
                 </Button>
             </div>
+            <DotPattern
+                className={cn(
+                "[mask-image:radial-gradient(1000px_circle_at_center,transparent,white)]",
+                )}
+            />
         </div>
     )
 }
