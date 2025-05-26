72756c9fb5e5e6f6b35aed49290b1223d63c2a9c
Andualem <andualem.fereja12@gmail.com>
2025-05-26 15:36:40 +0300

    feat(admin-ui): update product lists component

--- src/components/admin-ui/AdminRelatedProducts.tsx
+++ src/components/admin-ui/AdminRelatedProducts.tsx
@@ -1,12 +1,13 @@
 import { getProductsByCategory } from "@/lib/queries"
 import ProductCard from "./ProductCard"
+import { Product } from "@/lib/types"
 
 export default async function RelatedProducts({ id }:{ id: number }) {
     const { products } = await getProductsByCategory(String(id))
     return (
         <div className="flex space-x-3">
             {
-                products?.map((item:any, index:number) => (
+                products?.map((item:Product, index:number) => (
                     <div key={ index } className="w-[230px]">
                         <ProductCard product={ item } />
                     </div>
