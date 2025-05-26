d92752090ed8422d4f14d6b3b6c93de0e1d7e4f6
Andualem <andualem.fereja12@gmail.com>
2025-05-26 16:44:46 +0300

    fix: update auth-client

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,7 +1,8 @@
 import { createAuthClient } from "better-auth/react"
 import { adminClient } from "better-auth/client/plugins"
 
-const authClient = createAuthClient({ // the base url of your auth server
+const authClient = createAuthClient({
+    baseURL:"https://mencollection.vercel.app", // the base url of your auth server
     plugins: [
         adminClient()
     ]
