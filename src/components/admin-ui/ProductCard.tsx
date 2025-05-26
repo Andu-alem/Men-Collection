25603f372f61b84c4205bbac3b1e6c7708127aaa
Andualem <andualem.fereja12@gmail.com>
2025-05-26 16:29:32 +0300

    fix(admin-ui): update admin related product card

--- src/components/admin-ui/ProductCard.tsx
+++ src/components/admin-ui/ProductCard.tsx
@@ -17,7 +17,7 @@ const ProductCard = ({ product }:{ product: Product }) => {
     const { id, name, imagePath, price, quantity } = product
 
     return (
-        <Card className="border-zinc-200 shadow-none">
+        <Card className="border-zinc-200 dark:border-zinc-900">
             <CardContent className='p-2'>
                 <div className="relative w-full h-[150px]">
                     <Image
@@ -27,17 +27,17 @@ const ProductCard = ({ product }:{ product: Product }) => {
                         fill={ true }
                     />
                 </div>
-                <CardTitle className="my-2 text-zinc-700 capitalize">
+                <CardTitle className="my-2 text-zinc-700 dark:text-zinc-100 capitalize">
                     { name }
                 </CardTitle>
-                <p className="text-[15px] text-zinc-700">Price : { price }ETB</p>
-                <p className="text-[15px] text-zinc-700">In stock: { quantity }</p>
+                <p className="text-[15px] text-zinc-700 dark:text-zinc-200">Price : { price }ETB</p>
+                <p className="text-[15px] text-zinc-700 dark:text-zinc-200">In stock: { quantity }</p>
             </CardContent>
-            <CardFooter className="border-t border-gray-300 py-2 px-3 flex justify-evenly">
-                <Button className="text-[15px] border-zinc-400 h-7 px-1 sm:px-3 hover:text-orange-500" variant="outline" asChild>
+            <CardFooter className="border-t border-gray-300 dark:border-zinc-900 py-2 px-3 flex justify-evenly">
+                <Button className="text-[15px] border-zinc-400 h-7 px-1 sm:px-3 hover:text-amber-500" variant="outline" asChild>
                     <Link href="#">Update</Link>
                 </Button>
-                <Button className="cursor-pointer px-1 sm:px-3 hover:text-orange-500" variant="link" asChild>
+                <Button className="cursor-pointer px-1 sm:px-3 hover:text-amber-500" variant="link" asChild>
                     <Link href={`/admin/product/${id}`}>
                         <MoreHorizontal />
                     </Link>
