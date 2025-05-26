8c3b987dc01338bf270cc2963856401948384810
Andualem <andualem.fereja12@gmail.com>
2025-05-26 14:11:15 +0300

    feat: update add to cart buton

---
+++ src/components/AddToCart.tsx
@@ -0,0 +1,15 @@
+'use client'
+import { Button } from './ui/button'
+import { PlusIcon } from 'lucide-react'
+
+import { Product } from '@/lib/types'
+import { useCartStore } from '@/stores/store'
+
+export default function AddToCart({ product }:{ product:Product }) {
+    const addProduct = useCartStore((state) => state.addProduct)
+    return (
+        <Button className="py-0 h-7 border-zinc-400" variant="outline" onClick={ () => addProduct(product)}>
+            Cart<PlusIcon />
+        </Button>
+    )
+}
