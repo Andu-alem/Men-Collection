a028fc9e380cfc0c3871540cd1bd8280aab03ec4
Andualem <andualem.fereja12@gmail.com>
2025-05-26 15:49:21 +0300

    feat(app): update main app layout

--- src/app/(app)/cart/layout.tsx
+++ src/app/(app)/cart/layout.tsx
@@ -1,4 +1,10 @@
 
+import { Metadata } from "next"
+
+export const metadata:Metadata = {
+    title: "Cart"
+}
+
 export default function Layout({ children }:{ children:React.ReactNode }) {
     return (
         <div className="flex justify-center">
