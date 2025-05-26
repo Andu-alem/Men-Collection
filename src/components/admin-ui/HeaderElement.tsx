fd250bc864770edfab7395dba70e565eb6332069
Andualem <andualem.fereja12@gmail.com>
2025-05-26 16:28:59 +0300

    fix(admin-ui): update header element

--- src/components/admin-ui/HeaderElement.tsx
+++ src/components/admin-ui/HeaderElement.tsx
@@ -1,13 +1,19 @@
 'use client'
 import Link from 'next/link'
+import Image from 'next/image'
 import { authClient } from '@/lib/auth-client'
 import { Button } from '../ui/button'
+import ModeToggle from '../ModeToggle'
 
 export default function AdminHeader() {
     return (
-        <div className="sm:hidden flex-grow flex justify-between">
-            <Link href="/admin" className="font-bold">Men&apos;s Collection</Link>
-            <Button className="text-[15px] h-7 bg-zinc-700" onClick={ () => authClient.signOut() }>LOGOUT</Button>
+        <div className="sm:hidden flex-grow flex justify-between items-center">
+            <Link href="/admin" className="flex items-end">
+                <Image src="/logo2.png" width={40} height={40} alt="logo" />
+                <span className="font-semibold text-zinc-900 dark:text-zinc-200 text-xl sm:text-[15px] hover:underline">Classic Men</span>
+            </Link>
+            <ModeToggle />
+            <Button className="text-[15px] h-7 px-2 font-semibold" variant="secondary" onClick={ () => authClient.signOut() }>LOGOUT</Button>
         </div>
     )
 }
