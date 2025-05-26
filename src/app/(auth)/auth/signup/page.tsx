808700d5538552d2bbd579421507af280b36341b
Andualem <andualem.fereja12@gmail.com>
2025-05-26 16:38:10 +0300

    fix(auth): update signup form

--- src/app/(auth)/auth/signup/page.tsx
+++ src/app/(auth)/auth/signup/page.tsx
@@ -62,7 +62,7 @@ export default function Page() {
             }
           }, 
           onError: (ctx: any) => { 
-            toast(ctx.error.message); 
+            toast("Failed to signup. Please try again."); 
           }, 
         });
     }
