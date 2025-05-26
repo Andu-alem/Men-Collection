6ae6ab5f90b582fff898ce7d902a90c1505ed95f
Andualem <andualem.fereja12@gmail.com>
2025-05-26 14:13:26 +0300

    feat: udpate home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import HeroOutro from "@/components/HeroOutro"
 import Collections from "@/components/Collections"
 import Hero from "@/components/Hero"
 
-export default function Home() {
+export default async function Home() {
   return (
     <div className="">
       <Hero />
