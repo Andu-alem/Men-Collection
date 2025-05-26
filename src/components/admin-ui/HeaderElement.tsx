c043f240cdc3d20e84b0300bd2bbccc498c6f6fd
Andualem <andualem.fereja12@gmail.com>
2025-05-26 15:37:46 +0300

    feat(admin-ui): update admin header elements

--- src/components/admin-ui/HeaderElement.tsx
+++ src/components/admin-ui/HeaderElement.tsx
@@ -6,7 +6,7 @@ import { Button } from '../ui/button'
 export default function AdminHeader() {
     return (
         <div className="sm:hidden flex-grow flex justify-between">
-            <Link href="/admin" className="font-bold">Men's Collection</Link>
+            <Link href="/admin" className="font-bold">Men&apos;s Collection</Link>
             <Button className="text-[15px] h-7 bg-zinc-700" onClick={ () => authClient.signOut() }>LOGOUT</Button>
         </div>
     )
