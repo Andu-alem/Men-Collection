43b8d0d5985a41917c59641ca1354f5255de0c17
Andualem <andualem.fereja12@gmail.com>
2025-05-26 17:11:09 +0300

    feat(home-page): create orbiting section component to display categories with magic ui ripple and orbiting component

---
+++ src/components/OrbitingSection.tsx
@@ -0,0 +1,35 @@
+'use client'
+import { OrbitingCircles } from "./magicui/orbiting-circles"
+import { cn } from "@/lib/utils"
+import { DotPattern } from "./magicui/dot-pattern"
+import { Ripple } from "./magicui/ripple"
+import { AnimatedGradientText } from "./magicui/animated-gradient-text"
+
+export default function OrbitingSection() {
+    return (
+        <div className="relative bg-transparent flex h-screen w-full overflow-hidden flex-col items-center justify-center z-50">
+            <AnimatedGradientText className="text-white text-lg tracking-wide bg-slate-700">
+                Our Collections
+            </AnimatedGradientText>
+            <OrbitingCircles>
+                <span className="text-white font-semibold text-[20px]">Suits</span>
+                <span className="text-white font-semibold text-[20px]">Watches</span>
+                <span className="text-white font-semibold text-[20px]">Shoes</span>
+                <span className="text-white font-semibold text-[20px]">Shirts</span>
+                <span className="text-white font-semibold text-[20px]">Casual Clothings</span>
+            </OrbitingCircles>
+            <OrbitingCircles radius={270} reverse>
+                <span className="text-white font-semibold text-[20px]">Ties</span>
+                <span className="text-white font-semibold text-[20px]">Perfumes</span>
+                <span className="text-white font-semibold text-[20px]">Snickers</span>
+                <span className="text-white font-semibold text-[20px]">Sun Glasses</span>
+            </OrbitingCircles>
+            <Ripple mainCircleOpacity={ 0.3 } />
+            <DotPattern
+                className={cn(
+                "[mask-image:radial-gradient(350px_circle_at_center,white,transparent)]",
+                )}
+            />
+        </div>
+    )
+}
